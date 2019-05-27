87efb74350734bc9b5d88ce28669151a705e2b5b
Matthias <m@tth.es>
2019-05-27 18:00:37 +0200

    fix user email

--- src/VcsFactory.php
+++ src/VcsFactory.php
@@ -41,10 +41,10 @@ class VcsFactory
     public function repository(string $targetPath, string $repoUrl) : VcsRepository
     {
         if (preg_match("/^[a-z0-9_\-]+@[a-z0-9\-\.]+\:.*\.git$/", $repoUrl)) {
-            return new GitVcsRepository($repoUrl, $targetPath, $this->sshPrivKey);
+            return new GitVcsRepository($repoUrl, $targetPath, $this->commitUserName, $this->commitEmail, $this->sshPrivKey);
         }
         if (preg_match("/^http.*\.git$/", $repoUrl)) {
-            return new GitVcsRepository($repoUrl, $targetPath, $this->sshPrivKey);
+            return new GitVcsRepository($repoUrl, $targetPath, $this->commitUserName, $this->commitEmail, $this->sshPrivKey);
         }
         throw new \InvalidArgumentException("Cannot determine repository type: $repoUrl");
     }
