63b8ad9a616559f765b100f4da193b8a106539bc
Dominic-Andre Domröse <dominic.domroese@gmail.com>
2020-04-06 15:24:23 +0200

    differ between ssh and https git repository

--- src/VcsFactory.php
+++ src/VcsFactory.php
@@ -9,7 +9,11 @@
 namespace Phore\VCS;
 
 
+use InvalidArgumentException;
+use Phore\FileSystem\Exception\FilesystemException;
+use Phore\FileSystem\Exception\PathOutOfBoundsException;
 use Phore\FileSystem\PhoreTempFile;
+use Phore\VCS\Git\HttpsGitRepository;
 use Phore\VCS\Git\MockVcsRepository;
 use Phore\VCS\Git\SshGitRepository;
 
@@ -20,6 +24,8 @@ class VcsFactory
 
     private $commitUserName = "";
     private $commitEmail = "";
+    private $gitUser = "";
+    private $gitPassword = "";
 
     public function setAuthSshPrivateKey(string $privateKey)
     {
@@ -39,6 +45,12 @@ class VcsFactory
         return phore_exec("ssh-keygen -y -f :file", ["file" => $tmpfile->getUri()]);
     }
 
+    public function setGitUserAuth(string $gitUser, string $gitPassword)
+    {
+        $this->gitUser = $gitUser;
+        $this->gitPassword = $gitPassword;
+    }
+
     /**
      *
      * Note: public repositories need clone in http-mode if no ssh key is available.
@@ -46,22 +58,24 @@ class VcsFactory
      * @param string $targetPath
      * @param string $repoUrl
      * @return VcsRepository
+     * @throws FilesystemException
+     * @throws PathOutOfBoundsException
      */
     public function repository(string $targetPath, string $repoUrl): VcsRepository
     {
         if (preg_match("/^mock\@(.*)$/", $repoUrl, $matches)) {
             if (!DEV_MODE) {
-                throw new \InvalidArgumentException("Mock repositorys '$repoUrl' are allowed only in dev-mode");
+                throw new InvalidArgumentException("Mock repositories '$repoUrl' are allowed only in dev-mode");
             }
             return new MockVcsRepository($matches[1], $targetPath);
         }
         if (preg_match("/^[a-z0-9_\-]+@[a-z0-9\-\.]+\:.*$/", $repoUrl)) {
             return new SshGitRepository($repoUrl, $targetPath, $this->commitUserName, $this->commitEmail, $this->sshPrivKey);
         }
-        if (preg_match("/^http.*$/", $repoUrl)) {
-            return new SshGitRepository($repoUrl, $targetPath, $this->commitUserName, $this->commitEmail, $this->sshPrivKey);
+        if (preg_match("/^https.*$/", $repoUrl)) {
+            return new HttpsGitRepository($repoUrl, $targetPath, $this->commitUserName, $this->commitEmail, $this->gitUser,$this->gitPassword);
         }
-        throw new \InvalidArgumentException("Cannot determine repository type: $repoUrl");
+        throw new InvalidArgumentException("Cannot determine repository type: $repoUrl");
     }
 
 }
